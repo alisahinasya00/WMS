560c5c09f435cc12607b8887bbd68a68c23436d0
Adil Can Balcı <balciadilcan@gmail.com>
2024-03-22 19:02:09 +0300

    umutun amk

--- WMS.Business/Implementations/CalisanBs.cs
+++ WMS.Business/Implementations/CalisanBs.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Infrastructure.Utilities.ApiResponses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.Business.Interfaces;
+using WMS.DataAccess.Interfaces;
+using WMS.Model.Entities;
 
 namespace WMS.Business.Implementations
 {
-    internal class CalisanBs
+    public class CalisanBs : ICalisanBs
     {
+        private readonly ICalisanRepository _repo;
+
+        public CalisanBs(ICalisanRepository repo)
+        {
+            _repo= repo;
+        }
+
+        public async Task<ApiResponse<NoData>> DeleteAsync(int id)
+        {
+           
+
+            var calisan = await _repo.GetByIdAsync(id);
+            await _repo.DeleteAsync(calisan);
+
+        }
+
+        public Task<ApiResponse<List<Calisan>>> IdGoreCalisanGetir(params string[] includelist)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
