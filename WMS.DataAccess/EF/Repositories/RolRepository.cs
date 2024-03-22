2c1d09ae8b48f18f45ab9245929d5bda1389abec
Umut556 <umutduran221@gmail.com>
2024-03-22 17:46:42 +0300

    aa

---
+++ WMS.DataAccess/EF/Repositories/RolRepository.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.DataAccess.Implementations.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.DataAccess.EF.Contexts;
+using WMS.DataAccess.Interfaces;
+using WMS.Model.Entities;
+
+namespace WMS.DataAccess.EF.Repositories
+{
+    public class RolRepository : BaseRepository<Rol, WMSDbContext>, IRolRepository
+    {
+        public Task<List<Rol>> IdGoreGetir(int rolId, params string[] includeList)
+        {
+            return GetAsync(add => add.RolId == rolId, includeList);
+        }
+    }
+}
