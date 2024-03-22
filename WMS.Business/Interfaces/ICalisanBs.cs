e5cc43af8fa78ea0eaba2a9619a62c9b01b66cd0
Adil Can Balcı <balciadilcan@gmail.com>
2024-03-22 18:50:30 +0300

    dasd

---
+++ WMS.Business/Interfaces/ICalisanBs.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Utilities.ApiResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Model.Entities;
+
+namespace WMS.Business.Interfaces
+{
+    public interface ICalisanBs
+    {
+        Task<ApiResponse<List<Calisan>>> IdGoreCalisanGetir(params string[] includelist);
+
+    }
+}
