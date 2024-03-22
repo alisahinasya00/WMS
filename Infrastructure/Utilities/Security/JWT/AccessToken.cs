0d03057466ef74e69926a9b80faa4532ca265380
Umut556 <umutduran221@gmail.com>
2024-03-22 19:03:03 +0300

    aa

---
+++ Infrastructure/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Utilities.Security.JWT
+{ //  bu sınıfın nesenesini oluştrup tokeni talep eden client a göndereceğiz
+    public class AccessToken
+    {
+        public List<string> Claims { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ WMS.Business/Implementations/CalisanBs.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Utilities.ApiResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Business.Interfaces;
+using WMS.DataAccess.Interfaces;
+using WMS.Model.Entities;
+
+namespace WMS.Business.Implementations
+{
+    public class CalisanBs : ICalisanBs
+    {
+        private readonly ICalisanRepository _repo;
+
+        public CalisanBs(ICalisanRepository repo)
+        {
+            _repo= repo;
+        }
+
+        public async Task<ApiResponse<NoData>> DeleteAsync(int id)
+        {
+           
+
+            var calisan = await _repo.GetByIdAsync(id);
+            await _repo.DeleteAsync(calisan);
+
+        }
+
+        public Task<ApiResponse<List<Calisan>>> IdGoreCalisanGetir(params string[] includelist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
