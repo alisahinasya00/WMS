04fa3d49620c404c2bf71aeffe02bc98717b0922
Umut556 <umutduran221@gmail.com>
2024-03-22 16:05:47 +0300

    aa

--- WMS.Model/Entities/Rol.cs
+++ WMS.Model/Entities/Rol.cs
@@ -9,7 +9,7 @@ namespace WMS.Model.Entities
     public class Rol
     {
         public int RolId { get; set; }
-        public string RolAdi { get; set; }
+        public string RolAdi { get; set; }a
 
         public List <Calisan> Calisanlar { get; set; }
     }
