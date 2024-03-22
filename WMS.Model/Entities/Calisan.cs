2064c01ea13346c24ab6930f15f0f82db410bd31
Umut556 <umutduran221@gmail.com>
2024-03-22 15:48:36 +0300

    aa

---
+++ WMS.Model/Entities/Calisan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Model.Entities
+{
+    public class Calisan
+    {
+        public int CalisanId { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int Maas { get;  set; }
+        public string TelefonNo { get; set; }
+        public string Adres { get; set; }
+        public string Mail { get; set; }
+        public string Sifre { get; set; }
+        public DateTime IseGirisTarihi { get; set; }
+        public int RolId { get; set; }
+
+        public Rol Rol { get; set;}
+
+    }
+}
---
+++ WMS.Model/Entities/Rol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Model.Entities
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string RolAdi { get; set; }
+
+        public List <Calisan> Calisanlar { get; set; }
+    }
+}
