ba8d9e893e397fd5ced0739acfdd392fc567048d
Adil Can Balcı <balciadilcan@gmail.com>
2024-03-22 13:32:41 +0300

    asas

---
+++ Infrastructure/DataAccess/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Model;
+using System.Linq.Expressions;
+using System.Security.Principal;
+
+namespace Infrastructure.DataAccess.Interfaces
+{
+    public interface IBaseRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params string[] includeList);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params string[] includeList);
+        Task<TEntity> InsertAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+    }
+}
