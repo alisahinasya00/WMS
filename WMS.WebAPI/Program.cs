0f96274e656c671cda8451266e7b361c6b7c9114
Umut556 <umutduran221@gmail.com>
2024-03-24 15:51:25 +0300

    aa

--- WMS.WebAPI/Program.cs
+++ WMS.WebAPI/Program.cs
@@ -12,10 +12,7 @@ namespace WMS.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            builder.Services.AddScoped<IRolBs, RolBs>();
-            builder.Services.AddScoped<IRolRepository, RolRepository>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
