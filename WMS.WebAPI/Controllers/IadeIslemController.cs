e61cad2cb08079ac1ae256bfa3756b10899a4ea2
Umut556 <umutduran221@gmail.com>
2024-04-08 12:54:23 +0300

    aaa

---
+++ WMS.WebAPI/Controllers/IadeIslemController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WMS.WebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class IadeIslemController : BaseController
+    {
+        
+    }
+}
