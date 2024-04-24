24ed1515c114aa9fa90f64ec8d2fdfd1e3d8c951
Adil Can Balcı <balciadilcan@gmail.com>
2024-04-24 16:23:42 +0300

    hıbıo

---
+++ WMS.Model/Entities/IslemTur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Model.Entities
+{
+    public class IslemTur
+    {
+        public int IslemTurId { get; set; }
+        public string IslemAdi { get; set; }
+
+        public List<Islem>? Islemler { get; set; }
+        public List<GirisIslem>? GirisIslemler { get; set; }
+        public List<CikisIslem>? CikisIslemler { get; set; }
+        public List<IadeIslem>? IadeIslemler { get; set; }
+    }
+}
