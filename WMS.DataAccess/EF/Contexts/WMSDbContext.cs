f9830dc975be649e8c7437a8fb336ae83ef951ad
Umut556 <umutduran221@gmail.com>
2024-04-24 16:12:56 +0300

    rgsgrsgrs

--- WMS.DataAccess/EF/Contexts/WMSDbContext.cs
+++ WMS.DataAccess/EF/Contexts/WMSDbContext.cs
@@ -12,6 +12,19 @@ namespace WMS.DataAccess.EF.Contexts
 
         public DbSet<Calisan> Calisanlar { get; set; }
         public DbSet<Rol> Roller { get; set; }
+        public DbSet<Blok> Bloklar { get; set; }
+        public DbSet<Bolme> Bolmeler { get; set; }
+        public DbSet<Fabrika> Fabrikalar { get; set; }
+        public DbSet<GirisIslem> GirisIslemler { get; set; }
+        public DbSet<CikisIslem> CikisIslemler { get; set; }
+        public DbSet<IadeIslem> IadeIslemler { get; set; }
+        public DbSet<Islem> Islemler { get; set; }
+        public DbSet<IslemTuru> IslemTurleri { get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Konum> Konumlar { get; set; }
+        public DbSet<Magaza> Magazalar { get; set; }
+        public DbSet<Raf> Raflar { get; set; }
+        public DbSet<Urun> Urunler { get; set; }
 
     }
 }
