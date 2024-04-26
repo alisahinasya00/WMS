d8ad244307c79b0f5d856251d7c364a473719004
Umut556 <umutduran221@gmail.com>
2024-04-26 11:53:12 +0300

    afcafa

---
+++ WMS.Model/Dtos/Blok/BlokGetDto.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Model.Dtos.Blok
+{
+    public class BlokGetDto : IDto
+    {
+        public int BlokId { get; set; }
+        public string? BlokAdi { get; set; }
+    }
+}
