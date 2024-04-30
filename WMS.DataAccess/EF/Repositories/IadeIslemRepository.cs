e8bc0d782faded6ee093d60b86f4cebb7b44f9d4
alisahinasya00 <alisahinasya00@gmail.com>
2024-04-30 16:04:24 +0300

    aaa

--- WMS.DataAccess/EF/Repositories/IadeIslemRepository.cs
+++ WMS.DataAccess/EF/Repositories/IadeIslemRepository.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Infrastructure.DataAccess.Implementations.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.DataAccess.EF.Contexts;
+using WMS.DataAccess.Interfaces;
+using WMS.Model.Entities;
 
 namespace WMS.DataAccess.EF.Repositories
 {
-    public class IadeIslemRepository 
+    public class IadeIslemRepository : BaseRepository<IadeIslem, WMSDbContext>, IIadeIslemRepository
     {
+        public async Task<IadeIslem> IdGoreGetir(int iadeIslemId, params string[] includeList)
+        {
+            return await GetAsync(iadeIslem => iadeIslem.IadeIslemId == iadeIslemId, includeList);
+
+        }
+
+        public async Task<List<IadeIslem>> TariheGoreGetir(DateTime baslangicTarihi, DateTime bitisTarihi, params string[] includeList)
+        {
+            return await GetAllAsync(iadeIslem => iadeIslem.IslemTarihi >= baslangicTarihi && iadeIslem.IslemTarihi <= bitisTarihi, includeList);
+
+        }
     }
 }
