55dce347e651274312406d4e121f7cf6e34dade3
alisahinasya00 <alisahinasya00@gmail.com>
2024-05-01 12:38:59 +0300

    aa

---
+++ WMS.WebAPI/Controllers/GirisIslemController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WMS.Business.Implementations;
+using WMS.Business.Interfaces;
+using WMS.Model.Dtos.GirisIslem;
+
+namespace WMS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GirisIslemController : BaseController
+    {
+        private readonly IGirisIslemBs _girisIslemBs;
+
+        public GirisIslemController(IGirisIslemBs girisIslemBs)
+        {
+            _girisIslemBs = girisIslemBs;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetGirisIslemler()
+        {
+            var response= await _girisIslemBs.GirisIslemleriGetir();
+            return await SendResponseAsync(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> IdGoreGetir([FromRoute] int id)
+        {
+            var response=await _girisIslemBs.IdGoreGirisİslemGetir(id);
+            return await SendResponseAsync(response);
+        }
+
+        [HttpGet("Tarih")]
+        public async Task<IActionResult> TariheGoreGetir([FromQuery] DateTime baslangic, [FromQuery] DateTime bitis)
+        {
+            var response = await _girisIslemBs.TariheGoreGirisİslemGetir(baslangic, bitis);
+            return await SendResponseAsync(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveGirisIslem([FromBody] GirisIslemPostDto dto)
+        {
+            var response=await _girisIslemBs.Insert(dto);
+            if (response.ErrorMessages != null && response.ErrorMessages.Count > 0)
+            {
+                return await SendResponseAsync(response);
+            }
+            else
+                return CreatedAtAction(nameof(IdGoreGetir), new { id = response.Data.GirisIslemId }, response.Data);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateGirisIslem([FromBody] GirisIslemPutDto dto)
+        {
+            var response = await _girisIslemBs.Update(dto);
+
+            return await SendResponseAsync(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCalisan(int id)
+        {
+            var response = await _girisIslemBs.Delete(id);
+
+            return await SendResponseAsync(response);
+        }
+    }
+}
---
+++ WMS.WebAPI/Controllers/IslemController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WMS.Business.Implementations;
+using WMS.Business.Interfaces;
+using WMS.Model.Dtos.Calisan;
+using WMS.Model.Dtos.Islem;
+
+namespace WMS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IslemController : BaseController
+    {
+        private readonly IIslemBs _islemBs;
+
+        public IslemController(IIslemBs islemBs)
+        {
+            _islemBs = islemBs;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> IdGoreGetir([FromRoute] int id)
+        {
+            var response = await _islemBs.IdGoreIslemGetir(id);
+            return await SendResponseAsync(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetIslem()
+        {
+            var response = await _islemBs.IslemleriGetir();
+            return await SendResponseAsync(response);
+        }
+
+        [HttpGet("Tarih")]
+        public async Task<IActionResult> TariheGoreGetir([FromQuery] DateTime baslangic, [FromQuery] DateTime bitis)
+        {
+            var response = await _islemBs.TariheGoreIslemGetir(baslangic, bitis);
+            return await SendResponseAsync(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCalisan([FromBody] IslemPutDto dto)
+        {
+            var response = await _islemBs.UpdateAsync(dto);
+
+            return await SendResponseAsync(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCalisan(int id)
+        {
+            var response = await _islemBs.DeleteAsync(id);
+
+            return await SendResponseAsync(response);
+        }
+
+       
+    }
+}
