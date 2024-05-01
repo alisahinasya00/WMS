fd4626dc11abc71a9fc1b5bed296dbfec55a12f4
Umut556 <umutduran221@gmail.com>
2024-05-01 14:09:07 +0300

    vgvsg

--- WMS.Business/Profiles/KonumProfile.cs
+++ WMS.Business/Profiles/KonumProfile.cs
@@ -7,7 +7,13 @@ namespace WMS.Business.Profiles
     public class KonumProfile : Profile
     {
         public KonumProfile() {
-            CreateMap<Konum, KonumGetDto>();
+            CreateMap<Konum, KonumGetDto>()
+                .ForMember(dest => dest.BlokAdi,
+                       opt => opt.MapFrom(src => src.Bloklar.BlokAdi))
+                .ForMember(dest => dest.BolmeAdi,
+                       opt => opt.MapFrom(src => src.Bolme.BolmeAdi))
+                .ForMember(dest => dest.RafAdi,
+                           opt => opt.MapFrom(src => src.Raflar.RafAdi));
             CreateMap<KonumPutDto, Konum>();
             CreateMap<KonumPostDto, Konum>();
         }
