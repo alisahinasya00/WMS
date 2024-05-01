85c3177da78c72eecf923b18fe18e35fde74b973
Umut556 <umutduran221@gmail.com>
2024-05-01 13:45:47 +0300

    aaaaa

--- WMS.Business/Profiles/BolmeProfile.cs
+++ WMS.Business/Profiles/BolmeProfile.cs
@@ -7,7 +7,11 @@ namespace WMS.Business.Profiles
     public class BolmeProfile : Profile
     {
         public BolmeProfile() {
-            CreateMap<Bolme, BolmeGetDto>();
+            CreateMap<Bolme, BolmeGetDto>()
+                .ForMember(dest => dest.BlokAdi,
+                       opt => opt.MapFrom(src => src.Blok.BlokAdi))
+                .ForMember(dest => dest.RafAdi,
+                       opt => opt.MapFrom(src => src.Raf.RafAdi)); ;
             CreateMap<BolmePutDto, Bolme>();
             CreateMap<BolmePostDto, Bolme>();
 
