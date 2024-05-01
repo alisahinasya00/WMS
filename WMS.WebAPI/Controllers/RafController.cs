793c12c1790f697e2437b9c511c86d98f4d77cb7
Umut556 <umutduran221@gmail.com>
2024-05-01 14:56:39 +0300

    zvzdxv

--- WMS.WebAPI/Controllers/RafController.cs
+++ WMS.WebAPI/Controllers/RafController.cs
@@ -20,7 +20,7 @@ namespace WMS.WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetRaflar()
         {
-            var response = await _rafBs.GetRaflarAsync("Raf");
+            var response = await _rafBs.GetRaflarAsync("Blok");
             return await SendResponseAsync(response);
         }
 
@@ -28,14 +28,14 @@ namespace WMS.WebAPI.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> IdGoreGetir([FromRoute] int id)
         {
-            var response = await _rafBs.IdGoreRafGetirAsync(id, "Raf");
+            var response = await _rafBs.IdGoreRafGetirAsync(id, "Blok");
             return await SendResponseAsync(response);
         }
 
         [HttpGet("İsim")]
         public async Task<IActionResult> IsmeGoreGetir([FromQuery] string isim)
         {
-            var response = await _rafBs.IsimGoreRafGetirAsync(isim, "Raf");
+            var response = await _rafBs.IsimGoreRafGetirAsync(isim, "Blok");
             return await SendResponseAsync(response);
         }
 
