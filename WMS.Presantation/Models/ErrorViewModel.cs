00fc0c49f20ba2f328366fe2cddfa613eeb567c2
alisahinasya00 <alisahinasya00@gmail.com>
2024-05-10 14:00:10 +0300

    jj

---
+++ WMS.Presantation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WMS.Presantation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WMS.Model.Entities;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoginController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://example.com/api/Urun");
+            response.EnsureSuccessStatusCode();
+
+            // Stream olarak içeriği oku
+            Stream stream = await response.Content.ReadAsStreamAsync();
+
+            // Stream'ı JSON formatındaki veriye dönüştür
+            using (var reader = new StreamReader(stream))
+            {
+                string content = await reader.ReadToEndAsync();
+
+                // JSON verisini istediğiniz modele dönüştür
+                var products = JsonConvert.DeserializeObject<IEnumerable<Urun>>(content);
+
+                return View(products);
+            }
+        }
+    }
+}
