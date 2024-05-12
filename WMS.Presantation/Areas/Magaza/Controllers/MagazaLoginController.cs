afc05dd412b61a336ca7d047787f02fcf5616a49
Adil Can Balcı <balciadilcan@gmail.com>
2024-05-12 21:30:34 +0300

    magaza

--- WMS.Presantation/Areas/Magaza/Controllers/MagazaLoginController.cs
+++ WMS.Presantation/Areas/Magaza/Controllers/MagazaLoginController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using WMS.MvcUI.Areas.Admin.Controllers;
 using WMS.Presantation.Models;
+using WMS.MvcUI.Areas.Magaza.Models;
 
 namespace WMS.MvcUI.Areas.Magaza.Controllers
 {
@@ -14,15 +17,58 @@ namespace WMS.MvcUI.Areas.Magaza.Controllers
         {
             _logger= logger;
         }
-        public IActionResult MagazaLogin()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		private async Task<List<MagazaModel>> GetMagazaFromApi()
+		{
+			List<MagazaModel> magazaModels = new List<MagazaModel>();
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					client.BaseAddress = new Uri("http://localhost:5002/");
+					HttpResponseMessage response = await client.GetAsync("api/Magaza");
+					if (response.IsSuccessStatusCode)
+					{
+						var apiResponse = await response.Content.ReadAsStringAsync();
+						var responseData = JsonConvert.DeserializeObject<JObject>(apiResponse); // JSON verisini JObject olarak alın
+						var data = responseData["data"].ToString(); // "data" içindeki veriyi alın
+						magazaModels = JsonConvert.DeserializeObject<List<MagazaModel>>(data); // "data" içindeki veriyi liste olarak alın
+
+					}
+					else
+					{
+						ModelState.AddModelError(string.Empty, "API HATASI");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError(string.Empty, "Bir hata oluştu: " + ex.Message);
+			}
+			return magazaModels;
+		}
+
+
+		public async Task<IActionResult> MagazaLogin(string username, string password)
+		{
+			List<MagazaModel> magazalar = await GetMagazaFromApi();
+			var users = magazalar.FirstOrDefault(magaza => magaza.Mail == username && magaza.Sifre == password && magaza.RolAdi == "Magaza");
+			if (users != null)
+			{
+				TempData["UserName"] = users.Adi;
+				return RedirectToAction("MagazaPage", "MagazaPage");
+			}
+			else
+			{
+				ModelState.AddModelError(string.Empty, "Mağaza adı veya şifre hatalı.");
+				return View(magazalar);
+			}
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+	}
 }
---
+++ WMS.Presantation/Areas/Magaza/Controllers/MagazaPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WMS.MvcUI.Areas.Magaza.Controllers
+{
+	[Area("Magaza")]
+	public class MagazaPageController : Controller
+	{
+		public IActionResult MagazaPage()
+		{
+			return View();
+		}
+	}
+}
