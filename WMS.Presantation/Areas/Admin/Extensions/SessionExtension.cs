c8b177a0fb2532eb85f646001b35c19799fadb07
Adil Can Balcı <balciadilcan@gmail.com>
2024-05-19 21:54:16 +0300

    sdfdsfgdg

---
+++ WMS.Presantation/Areas/Admin/Extensions/SessionExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace WMS.MvcUI.Areas.Admin.Extensions
+{
+    public static class SessionExtension
+    {
+        public static void SetObject(this ISession session, string key, object data)
+        {
+            session.SetString(key, JsonSerializer.Serialize(data));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var sessionJsonData = session.GetString(key);
+            if (sessionJsonData != null)
+            {
+                return JsonSerializer.Deserialize<T>(sessionJsonData);
+            }
+            else
+            {
+                // sessionJsonData null ise, uygun bir işlem yapılabilir.
+                // Örneğin, varsayılan bir değer döndürebilir veya hata fırlatabiliriz.
+                throw new ArgumentNullException(nameof(sessionJsonData), "Session data is null for the specified key.");
+                // veya
+                // return default(T); // Varsayılan değeri döndür
+            }
+        }
+
+    }
+}
---
+++ WMS.Presantation/Areas/Admin/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using WMS.MvcUI.Areas.Admin.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using WMS.MvcUI.Areas.Admin.Models;
+
+namespace WMS.MvcUI.Areas.Admin.ViewComponents
+{
+    public class SideBarViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            var adminUser = HttpContext.Session.GetObject<AdminModelItem>("ActiveAdminUser");
+
+            return View(adminUser);
+        }
+    }
+}
